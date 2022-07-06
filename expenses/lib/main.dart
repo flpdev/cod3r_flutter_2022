ad257430facb4971ddfbdcbb09d75da38ccd3d1f
unknown <fm.felipe@gmail.com>
2022-07-06 15:44:46 -0400

    aula 10

--- expenses/lib/main.dart
+++ expenses/lib/main.dart
@@ -63,7 +63,7 @@ class MyhomePage extends StatelessWidget {
                         ),
                         padding: EdgeInsets.all(10),
                         child: Text(
-                          tr.value.toString(),
+                          'R\$ ${tr.value.toStringAsFixed(2)}',
                           style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
