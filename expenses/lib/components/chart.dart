5bff521017811e13d1b2f4e6f645d02376796a2d
unknown <fm.felipe@gmail.com>
2022-07-07 14:30:11 -0400

    aula 19

---
+++ expenses/lib/components/chart.dart
@@ -0,0 +1,42 @@
+import 'package:expenses/Model/transaction.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class Chart extends StatelessWidget {
+  final List<Transaction> recenteTransactions;
+  Chart(this.recenteTransactions);
+
+  List<Map<String, Object>> get groupedTransactions {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(
+        Duration(days: index),
+      );
+
+      double totalSum = 0.0;
+
+      for (var i = 0; i < recenteTransactions.length; i++) {
+        bool sameDay = recenteTransactions[i].date.day == weekDay.day;
+        bool sameMonth = recenteTransactions[i].date.month == weekDay.month;
+        bool sameYear = recenteTransactions[i].date.year == weekDay.year;
+
+        if (sameDay && sameMonth && sameYear) {
+          totalSum += recenteTransactions[i].value;
+        }
+      }
+
+      return {
+        'day': DateFormat.E().format(weekDay)[0],
+        'value': totalSum,
+      };
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 6,
+      margin: EdgeInsets.all(20),
+      child: Row(children: []),
+    );
+  }
+}
