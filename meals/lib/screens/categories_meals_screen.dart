e13c2b73983011f4a4311ecc3a685b2ac0e64bff
unknown <fm.felipe@gmail.com>
2022-07-12 23:49:32 -0400

    meals aula 10

--- meals/lib/screens/categories_meals_screen.dart
+++ meals/lib/screens/categories_meals_screen.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/category.dart';
 
+import '../data/dummy_data.dart';
+
 class CategoriesMealsScreen extends StatelessWidget {
   // const CategoriesMealsScreen(this.category);
 
   @override
   Widget build(BuildContext context) {
     final category = ModalRoute.of(context)!.settings.arguments as Category;
+
+    final categoryMeals = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text('Receitas por categoria'),
+      body: ListView.builder(
+        itemCount: categoryMeals.length,
+        itemBuilder: (context, index) {
+          return Text(categoryMeals[index].title);
+        },
       ),
     );
   }
