5e1138cf62b70c40b47d349f7081612d145c80c7
Felipe Moreira <felipe.oliveira@mt.senac.br>
2022-07-15 13:18:20 -0400

    meals aula 13

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,6 +1,9 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
 import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/screens/meal_detail_screen.dart';
 import 'package:meals/utils/app_routes.dart';
 
 void main() => runApp(MyApp());
@@ -27,6 +30,7 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.HOME: (ctx) => CategoriesScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.MEAL_DERAIL: (ctx) => MealDetailScreen()
       },
     );
   }
