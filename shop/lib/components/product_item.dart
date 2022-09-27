3f3137f586c3eb19dd5e66971666cf03a2175e18
Felipe Moreira <felipe.oliveira@mt.senac.br>
2022-09-27 16:15:02 -0400

    aula 11 Product Favorite

--- shop/lib/components/product_item.dart
+++ shop/lib/components/product_item.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/utils/app_routes.dart';
 
+import '../models/product.dart';
+
 class ProductItem extends StatelessWidget {
-  final product;
-  const ProductItem({Key? key, required this.product}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -13,13 +16,17 @@ class ProductItem extends StatelessWidget {
           title: Text(product.title),
           backgroundColor: Colors.black87,
           leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.favorite),
+            onPressed: () {
+              product.toggleFavorite();
+            },
+            icon: Icon(
+              product.isFavorite ? Icons.favorite : Icons.favorite_border,
+            ),
             color: Theme.of(context).colorScheme.secondary,
           ),
           trailing: IconButton(
             onPressed: () {},
-            icon: const Icon(Icons.shopping_cart),
+            icon: Icon(Icons.shopping_cart),
             color: Theme.of(context).colorScheme.secondary,
           ),
         ),
